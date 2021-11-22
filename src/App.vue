15e20ef662eac6640405945655ae04ba1d425948
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-11-22 19:11:47 +0100

    Aggiunte navbar nella sezione header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    
+    <contact-header-band></contact-header-band>
   </div>
 </template>
 
 <script>
+import ContactHeaderBand from './components/ContactHeaderBand.vue'
 
 export default {
   name: 'App',
   components: {
+    ContactHeaderBand
 
   }
 }
---
+++ src/components/ContactHeaderBand.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+      <div class="justify-content-evenly flex-row">
+        <div class="d-flex align-items-center">
+            <i class="far fa-clock"></i>
+            <div>Open Hours: Mon - Sat 9:00 - 18.00</div>
+        </div>
+        <div class="d-flex align-items-center">
+            <div>
+                <i class="fas fa-phone-alt d-inline-block"></i>
+                <div class="d-inline-block">+1 (305) 1234-5678</div>
+            </div>
+            <div>
+                <i class="far fa-envelope d-inline-block"></i>
+                <div class="d-inline-block">hello@example.com</div>
+            </div>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-linkedin-in"></i>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .container-fluid {
+        background-color: #21333E;
+        color: white;
+        font-size: 15px;
+    }
+
+</style>
