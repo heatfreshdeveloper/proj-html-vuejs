560e591b3d8fa50791638c790f76a3cafae92bc1
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-11-23 16:34:04 +0100

    Aggiunta Navbar e immagini css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <contact-header-band></contact-header-band>
+    <Header></Header>
   </div>
 </template>
 
 <script>
 import ContactHeaderBand from './components/ContactHeaderBand.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    ContactHeaderBand
+    ContactHeaderBand,
+    Header,
 
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="my-header">
+      <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <div class="container-fluid">
+                <img class="navbar-brand" href="#" />
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li v-for="nav in navList" :key="nav.items" class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">{{ nav.items }}</a>
+                    </li>
+                </ul>
+              </div>
+            </nav>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            navList: [
+                {items: "HOME"}, 
+                {items: "ABOUT"}, 
+                {items: "SERVICES"}, 
+                {items: "PROCESS"},
+                {items: "TEAM"}, 
+                {items: "BLOG"}, 
+                {items: "GET IN TOUCH"} 
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+    .my-header {
+        background-image: url(../assets/bg-6.jpg);
+        
+    }
+</style>
