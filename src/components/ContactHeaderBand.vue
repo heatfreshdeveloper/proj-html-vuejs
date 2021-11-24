9f31bf21330d4ee5426a63ab335766b4b8137d94
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-11-24 16:45:10 +0100

    Completato components header

--- src/components/ContactHeaderBand.vue
+++ src/components/ContactHeaderBand.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="container-fluid">
-      <div class="justify-content-evenly flex-row">
+  <div class="container-fluid my-header-band">
+      <div class="d-flex py-2 justify-content-evenly flex-row">
         <div class="d-flex align-items-center">
-            <i class="far fa-clock"></i>
+            <i class="far fa-clock me-2"></i>
             <div>Open Hours: Mon - Sat 9:00 - 18.00</div>
         </div>
         <div class="d-flex align-items-center">
             <div>
-                <i class="fas fa-phone-alt d-inline-block"></i>
-                <div class="d-inline-block">+1 (305) 1234-5678</div>
+                <i class="fas fa-phone-alt d-inline-block me-2"></i>
+                <div class="d-inline-block me-3">+1 (305) 1234-5678</div>
             </div>
             <div>
-                <i class="far fa-envelope d-inline-block"></i>
-                <div class="d-inline-block">hello@example.com</div>
+                <i class="far fa-envelope d-inline-block me-2"></i>
+                <div class="d-inline-block me-3">hello@example.com</div>
             </div>
-            <i class="fab fa-facebook"></i>
-            <i class="fab fa-twitter"></i>
-            <i class="fab fa-linkedin-in"></i>
+            <i class="fab fa-facebook me-2"></i>
+            <i class="fab fa-twitter me-2"></i>
+            <i class="fab fa-linkedin-in me-2"></i>
         </div>
       </div>
   </div>
@@ -29,7 +29,7 @@ export default {
 </script>
 
 <style>
-    .container-fluid {
+    .my-header-band {
         background-color: #21333E;
         color: white;
         font-size: 15px;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="my-header">
-      <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="my-container">
+          <nav class="navbar navbar-expand-lg navbar-light">
               <div class="container-fluid">
-                <img class="navbar-brand" href="#" />
+                <img class="navbar-brand" href="#" src="../assets/image.png"/>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li v-for="nav in navList" :key="nav.items" class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">{{ nav.items }}</a>
@@ -11,6 +11,39 @@
                 </ul>
               </div>
             </nav>
+            <div class="consulting-box">
+                <div class="my-titles">
+                    <p>FUSIONS & ACQUISITIONS</p>
+                </div>
+                <div class="main-title">
+                    <h1>Insurance 
+                        <br>Consulting</h1>
+                </div>
+                <div class="row row-cols-2 gy-3">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Phone">
+                    </div>
+                    <div class="col">
+                        <div class="accordion-item">
+                            <div class="accordion-header">
+                                <button class="accordion-button arrow-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    More info
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="consulting-buttons mt-3">
+                    <button type="button" class="btn btn-success mx-2">GET IN TOUCH</button>
+                    <button type="button" class="btn btn-outline-success">READ MORE</button>
+                </div>
+            </div>
       </div>
   </div>
 </template>
@@ -37,6 +70,28 @@ export default {
 <style>
     .my-header {
         background-image: url(../assets/bg-6.jpg);
-        
+        background-position: 30% 30%;
+        min-height: 800px;
+        position: relative;
+    }
+
+    .my-container {
+        padding-left: 280px;
+        padding-right: 280px;
+    }
+
+    .consulting-box {
+        position: absolute;
+        top: 30%;
+        right: 20%;
+        max-width: 450px;
+    }
+
+    .my-titles {
+        color: #157347;
+    }
+
+    .arrow-button {
+        height: 37px;
     }
 </style>
